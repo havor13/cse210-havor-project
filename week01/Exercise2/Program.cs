788d72ff8438246b6224c522c9b61e419ce0fd7c
Sampson Havor <sampsonhavor43@gmail.com>
2025-05-09 15:13:05 -0700

    finished exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -2,8 +2,63 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Ask user for their grade percentage
+        Console.Write("Enter your grade percentage: ");
+        int percentage = int.Parse(Console.ReadLine());
+
+        string letter = "";
+        string sign = "";
+
+        // Determine the letter grade
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determine the "+" or "-" sign
+        int lastDigit = percentage % 10;
+
+        if (letter != "A" && letter != "F")  // A+ and F+/F- do not exist
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        // Print the final grade
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        // Congratulate or encourage the user
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up! Keep working hard for next time.");
+        }
     }
 }
